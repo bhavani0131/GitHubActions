2208bc8c48fc5e59a0773489bc5517896047a6c1
bhavani0131 <durgabhavaniv77@gmail.com>
2026-06-26 19:22:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,42 @@ resource "aws_instance" "dev" {
       Name = "Bhavani"
     }
 }
+
+
+# terraform {
+#   backend "s3" {
+#     bucket = "bhavani-terraform-state-bucket"   # Replace with your bucket name
+#     key    = "terraform.tfstate"
+#     region = "ap-south-2"
+#   }
+# }
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "bhavani-terraform-state-bucket"   # Must be globally unique
+
+  tags = {
+    Name = "Terraform State Bucket"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
+
+
+
+
+
