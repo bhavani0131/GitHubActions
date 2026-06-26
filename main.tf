f180d522761ae60fdfc1f8f9af2e51e3ee799ca3
bhavani0131 <durgabhavaniv77@gmail.com>
2026-06-26 13:31:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
- region = "us-west-2"
+ region = "ap-south-2"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0cb473a1f3c06c13d"
+    ami = "ami-01f3f4b95d125a9af"
     instance_type = "t3.micro"
     tags = {
       Name = "Bhavani"
