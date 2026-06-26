9230be5ba60d56fa04f58d72150e4d502d5c8a94
bhavani0131 <durgabhavaniv77@gmail.com>
2026-06-26 20:03:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+ region = "ap-south-2"
 }
 
 resource "aws_instance" "dev" {
@@ -10,38 +11,38 @@ resource "aws_instance" "dev" {
 }
 
 
-# terraform {
-#   backend "s3" {
-#     bucket = "bhavani-terraform-state-bucket"   # Replace with your bucket name
-#     key    = "terraform.tfstate"
-#     region = "ap-south-2"
-#   }
-# }
+ terraform {
+   backend "s3" {
+     bucket = "bhavani-terraform-state-bucket"   # Replace with your bucket name
+     key    = "terraform.tfstate"
+     region = "ap-south-2"
+   }
+ }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "bhavani-terraform-state-bucket"   # Must be globally unique
+#resource "aws_s3_bucket" "terraform_state" {
+#  bucket = "bhavani-terraform-state-bucket"   # Must be globally unique
 
-  tags = {
-    Name = "Terraform State Bucket"
-  }
-}
+#  tags = {
+#    Name = "Terraform State Bucket"
+#  }
+#}
 
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
+#resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+#  bucket = aws_s3_bucket.terraform_state.id
 
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# versioning_configuration {
+#    status = "Enabled"
+#  }
+#}
 
-resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
-  bucket = aws_s3_bucket.terraform_state.id
+#resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
+#  bucket = aws_s3_bucket.terraform_state.id
 
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-}
+#  block_public_acls       = true
+#  ignore_public_acls      = true
+#  block_public_policy     = true
+#  restrict_public_buckets = true
+#}
 
 
 
